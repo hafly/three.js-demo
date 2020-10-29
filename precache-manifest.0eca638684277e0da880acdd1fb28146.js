63f0b1ab7cb8c3d6d087a72a163d5c8ce0b4f86c
hafly <188150920@qq.com>
2020-10-29 09:47:14 +0800

    Updates

---
+++ precache-manifest.0eca638684277e0da880acdd1fb28146.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac038f2fdca024ab8648a1f6510afbc7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7521965804be28ff4db",
+    "url": "/static/css/main.f393f302.chunk.css"
+  },
+  {
+    "revision": "af586157f175c1d207a1",
+    "url": "/static/js/2.c87fb02e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c87fb02e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7521965804be28ff4db",
+    "url": "/static/js/main.2431baa8.chunk.js"
+  },
+  {
+    "revision": "abe7a5b848b8bb15400e",
+    "url": "/static/js/runtime-main.f00d07c8.js"
+  },
+  {
+    "revision": "4f4d986a24bc2f3f161badfe5d90da42",
+    "url": "/static/media/guang2.4f4d986a.jpg"
+  },
+  {
+    "revision": "381c728e671d102dc9629ba66b9e7b03",
+    "url": "/static/media/world.381c728e.png"
+  }
+]);
